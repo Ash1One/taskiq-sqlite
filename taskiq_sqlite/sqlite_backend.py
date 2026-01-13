948b85eeba0da81de373e1337cb2032f34aa3641
huang hao <hao.huang@dinfectome.com>
2026-01-13 10:28:39 +0800

    fix: update import statement for exceptions in SQLiteResultBackend

--- taskiq_sqlite/sqlite_backend.py
+++ taskiq_sqlite/sqlite_backend.py
@@ -14,7 +14,7 @@ from taskiq.compat import model_dump, model_validate
 from taskiq.result import TaskiqResult
 from taskiq.serializers import PickleSerializer
 
-from taskiq_sqlite.exceptions import (
+from .exceptions import (
     DuplicateExpireTimeSelectedError,
     ExpireTimeMustBeMoreThanZeroError,
     ResultIsMissingError,
